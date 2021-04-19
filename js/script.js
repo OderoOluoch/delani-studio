2477eb3e64d5f00977650b000c62d75661ab6a90
OderoOluoch <oderoluoch@gmail.com>
2021-04-19 14:19:07 +0300

    Working on the hover effect

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
     $("#dtext").hide()
     $("#design-img").click(function(){
         $("#dtext").show(1000);
-        $("#design-img").hide();
+        $("#design-img").hide(1000);
         $("#dtext").click(function(){
-            $("#design-img").show()
-            $("#dtext").hide()
+            $("#design-img").show(1000)
+            $("#dtext").hide(1000)
         })
     })
 })
@@ -15,22 +15,29 @@ $(document).ready(function(){
     $("#dev").hide()
     $("#dev-img").click(function(){
         $("#dev").show(1000);
-        $("#dev-img").hide();
+        $("#dev-img").hide(1000);
         $("#dev").click(function(){
-            $("#dev-img").show()
-            $("#dev").hide()
+            $("#dev-img").show(1000)
+            $("#dev").hide(1000)
         })
     })
 })
 
-$(document).ready(
-    $("#dev-img").click(function(){
-        $("#dev").toggle(1000)
-    })
-)
-
-$(document).ready(
+$(document).ready(function(){
+    $("#prod").hide()
     $("#prod-img").click(function(){
-        $("#prod").toggle(1000)
+        $("#prod").show(1000);
+        $("#prod-img").hide(1000);
+        $("#prod").click(function(){
+            $("#prod-img").show(1000)
+            $("#prod").hide(1000)
+        })
     })
-)
+})
+
+
+$(document).ready(function(){
+    $("#img-Hov").hover(function(){
+        alert("Hoverr")
+    })
+})
