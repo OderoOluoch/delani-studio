8ad669dfb8221eb5b5a7a50ffa20704e93d51d71
OderoOluoch <oderoluoch@gmail.com>
2021-04-19 13:34:30 +0300

    Full functions working for the first element

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-$(document).ready(
+$(document).ready(function(){
+    $("#dtext").hide()
     $("#design-img").click(function(){
-        $("#dtext").toggle(1000)
+        $("#dtext").show(1000);
+        $("#design-img").hide();
+        $("#dtext").click(function(){
+            $("#design-img").show()
+            $("#dtext").hide()
+        })
     })
-)
+})
+
+
+$(document).ready(function(){
+    $("#dtext").hide()
+    $("#design-img").click(function(){
+        $("#dtext").show(1000);
+        $("#design-img").hide();
+        $("#dtext").click(function(){
+            $("#design-img").show()
+            $("#dtext").hide()
+        })
+    })
+})
 
 $(document).ready(
     $("#dev-img").click(function(){
