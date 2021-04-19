942d7b0a40ac16ed5c7eed52de48b7435cbc1886
OderoOluoch <oderoluoch@gmail.com>
2021-04-19 13:26:50 +0300

    Trying out the jQuery part

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    $("#design-img").click(function(){
+        $("#dtext").toggle(1000)
+    })
+)
+
+$(document).ready(
+    $("#design-img").click(function(){
+        $("#dtext").toggle(1000)
+    })
+)
